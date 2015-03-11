e62d6cbc50b2db8767757b633e999293816d047d
Constantin <ckraft@smail.uni-koeln.de>
2015-03-11 12:04:48 +0100

    processXML implemented: mustache templating, Event-handler binding

--- public/js/mediathek.js
+++ public/js/mediathek.js
@@ -23,11 +23,21 @@ $(document).ready(function() {
             // Container versteckt halten
             config.container.hide();
             
+            // load Mustache template
+            $.Mustache.load('templates/info-table.html')
+                .done(function() {
+                    console.log('template loaded…');
+                });
+            $.Mustache.load('templates/media-object.html')
+                .done(function() {
+                    console.log('template loaded…');
+                });
+                
             // setup html container
             // getRecordsFromServer(callback)
             getRecordsFromServer(processXML);
 
-            // 
+
         }
 
         var getRecordsFromServer = function(callback) {
@@ -51,36 +61,82 @@ $(document).ready(function() {
             // TODO: selector feintunen: nur media-objects
             $elements = $xml.find('image, document');
 
+            // iterate XML elements
             $.each($elements, function( idx, el ) {
+                // 1) Über alle Elemente iterieren
+                // 2) Views vorbereiten
+                // 3) Templates rendern
                 $el = $(el);
-                console.log($el);
+                //console.log($el);
                 var filename = $el.find("filename").text();
                 // DEBUG
                 console.log("filename: " + filename);
 
-                var infoTableView;
-                $.each($el.children(), function( idx, el ) {
-                    console.log($(el).tagName);
-                    // Kind-Elemente von MediaObjekt mit infoTableView mergen:
-                    // $.extend
-                });
+                // TODO: stattdessen über XML-Elemente
+                // iterieren, da hässlich, redundandt 
+                var infoTableView = {
+                    "filetype": "",
+                    "filedate": "",
+                    "filename": "",
+                    "filesize": "",
+                    "title": "",
+                    "description": "",
+                    "camera-make": "",
+                    "camera-model": "",
+                    "resolution": "",
+                    "iso-speed": "",
+                    "exposure-time": "",
+                    "flash-used": "",
+                    "keywords": "",
+                    "city": "",
+                    "country": "",
+                    "author": "",
+                }
+                for (var property in infoTableView) {
+                    infoTableView[property] = $el.find(property).text();                 
+                }
+
+                console.debug(infoTableView);
+                
                 var mediaObjectsView = {
                     "filename": filename,
                     "thumbfile": function() {
                         if( infoTableView.filetype == "image/jpeg" ) {
-                            return config.file_thumbspath + this.filename;
+                            // FIXME: config-Variablen nicht im scope
+                            //return config.file_thumbspath + this.filename;
+                            return "/mediathek/public/img/thumbnails/" +
+                                this.filename;
                         } else {
-                            return config.file_thumbspath + "defaultthumbnail.png";
+                            return "/mediathek/public/img/thumbnails/" +
+                                "defaultthumbnail.png";
                         }
                     }
                 };
+                var $item = $($.Mustache.render('media-object', mediaObjectsView));
+                // render info table
+                var infoTable = $($.Mustache.render('info-table', infoTableView));
                 
-                // 1) Über alle Elemente iterieren
-                // 2) Daten in Template einfügen
-                // 3) Templates rendern
+                infoTable.appendTo('#hidden-infos');
+                //console.log($item.html());
+                
+                // TODO: bind event handler
+                $('div#content .container').append($item);
+                $('.info-container').hide();
+                // bind event handler
+                console.log($("a.more-link"));
+                $('a.more-link').click(function( e ){
+                    console.log("bam!");
+                    // Get index of current item to access info-container in hidden div
+                    idx = $(this).parent().index();
+                    //$('#hidden-infos :eq(' + idx + ')').fadeIn('slow');
+                    $('#hidden-infos div').eq(idx).fadeIn('slow')
+                        .click(function(){ $('#hidden-infos div').eq(idx).fadeOut('fast'); });
+                });
             });
-
-
+        };
+        
+        var showInfoPopup = function() {
+            // TODO:
         };
 
         init( {} );
